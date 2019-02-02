1097ba9e2f0ba0958bc1196af32c116fedc0b9aa
KathrynPare <47276325+KathrynPare@users.noreply.github.com>
2019-02-02 16:37:56 -0400

    Update GroceryList.java

--- GroceryList.java
+++ GroceryList.java
@@ -1,3 +1,42 @@
 public class GroceryList {
-
+	// ----- Instance Variables ----- //
+	ArrayList<String> checkList;
+	
+	// ----- Constructor ----- //
+	public GroceryList() {
+		
+	}
+	
+	// ---- Methods ----- //
+	//Uploads the saved checklist by reading the checkList from a file
+	private void uploadChecklist() {
+		
+	}
+	
+	//Saves the current checklist by printing checklist to a file
+	private void saveChecklist() {
+		
+	}
+	
+	//Adds new item to the checklist
+	private void addToChecklist() {
+		
+	}
+	
+	//Deletes the item without adding it to the Pantry
+	private void deleteWithoutAddingToPantry(String item) {
+		
+	}
+	
+	//Deletes the item from the checklist
+	//and called addtoPantry(String) to add it to the Pantry
+	private void deleteAndAddToPantry(String item) {
+		
+	}
+	
+	// ----- Helper Methods ----- //
+	//Takes in a string, converts it to a PantryItem, and gives the new PantryItem to the Pantry
+	private void addToPantry(String newPI) {
+		
+	}
 }
