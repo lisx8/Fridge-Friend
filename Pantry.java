4d75ae5e3313d160743ae7cd574f6b47be4636c2
opixe <47275861+opixe@users.noreply.github.com>
2019-02-02 16:43:53 -0400

    minor edit
    i changed the constructor from public void PantryItem(String item) to public PantryItem(String item)
    
    it makes my code work
    
    pls

--- Pantry.java
+++ Pantry.java
@@ -1,3 +1,67 @@
-public class Pantry {
+import java.io.*;
+import java.util.Scanner;
+import java.util.PriorityQueue;
+import java.util.Collections;
 
+public class Pantry
+{
+	private static PriorityQueue<PantryItem> sorted;
+	private static File inFile;
+	private static int numPantryList;
+	private static Scanner scanFile;
+	private static PantryItem newGrocery;
+	
+	//Constructor for Pantry; takes an array of PantryItems and does [INSERT STUFF HERE]
+	public Pantry()
+	{
+		if(this.findFile())
+		{
+			//If file is found, continue doing your shit
+		}
+	}
+	
+	private boolean findFile()
+	{
+		inFile = new File("C:/Users/Shay/Documents/PantryList.txt");
+		
+		try
+		{
+			scanFile = new Scanner(inFile);
+			numPantryList = scanFile.nextInt();
+			
+			return true;
+		} catch(Exception e)
+		{
+			System.out.println("File not found.");
+			
+			return false;
+		}
+	}
+	
+	public static void minHeapSort(PantryItem[] unsorted)
+	{
+		sorted = new PriorityQueue<PantryItem>(unsorted.length);
+		
+		newGrocery = new PantryItem(scanFile.next());
+		sorted.add(newGrocery);
+	}
+	
+	/*public static void addToPantry(PantryItem food)
+	{
+		
+	}*/
+	
+	
+	
+	
+	
+	
+	
+	/* When user presses delete button, the String is passed from the GUI to this method, where
+	 * it will be searched for in the sorted array (NAME IT) and then deleted. 
+	 */
+	public static void pantryDeleter(String del)
+	{
+		
+	}
 }
--- PantryItem.java
+++ PantryItem.java
@@ -1,3 +1,34 @@
-public class PantryItem {
-
+public class PantryItem implements Comparable<PantryItem>{
+	private String pantryItem;
+	PantryItemDatabase database = new PantryItemDatabase();
+	private int numDaysToExpiry;
+	public PantryItem(String item)
+	{
+		pantryItem = item;
+		numDaysToExpiry = database.checkDatabase(item);
+	}
+	
+	public int compareTo(PantryItem other)
+	{
+		if(this.getNumDaysToExpiry() < other.getNumDaysToExpiry())
+		{
+			return -1;
+		}
+		else if(this.getNumDaysToExpiry() == other.getNumDaysToExpiry())
+		{
+			return 0;
+		}
+		else
+		{
+			return -1;
+		}
+	}
+	public String pantryItem()
+	{
+		return pantryItem;
+	}
+	public int getNumDaysToExpiry()
+	{
+		return numDaysToExpiry;
+	}
 }
