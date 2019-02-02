f0d4c092dbd96c2c1c59c2d14f42d0df0ce6cd4a
KathrynPare <47276325+KathrynPare@users.noreply.github.com>
2019-02-02 18:38:41 -0400

    Update GroceryList.java

--- GroceryList.java
+++ GroceryList.java
@@ -1,15 +1,19 @@
+import java.util.*;
+import java.io.*;
+
 public class GroceryList {
 	// ----- Instance Variables ----- //
-	ArrayList<String> checkList;
+	private static ArrayList<String> checklist;
 	
 	// ----- Constructor ----- //
-	public GroceryList() {
-		
+	public GroceryList() throws FileNotFoundException{
+		checklist = new ArrayList<String>();
+		this.uploadChecklist();
 	}
 	
 	// ---- Methods ----- //
 	//Uploads the saved checklist by reading the checkList from a file
-	private void uploadChecklist() {
+	private void uploadChecklist() throws FileNotFoundException {
 		
 	}
 	
@@ -19,24 +23,26 @@ public class GroceryList {
 	}
 	
 	//Adds new item to the checklist
-	private void addToChecklist() {
-		
+	//In the future, we might make this boolean, because if the checklist already contains the item
+	//We'll want to tell the user it already exists
+	private static void addToChecklist(String newItem) {
+		//Do we want the checklist sorted? if so, we would probably sort them as we add them to the checklist
+		if (!checklist.contains(newItem)) {
+			checklist.add(newItem);
+		}
 	}
 	
 	//Deletes the item without adding it to the Pantry
-	private void deleteWithoutAddingToPantry(String item) {
-		
+	private static void deleteWithoutAddingToPantry(String item) {
+		checklist.remove(item);
 	}
 	
 	//Deletes the item from the checklist
-	//and called addtoPantry(String) to add it to the Pantry
-	private void deleteAndAddToPantry(String item) {
-		
+	//and calls Pantry.addtoPantry(String) to add it to the Pantry
+	private static void deleteAndAddToPantry(String item) {
+		PantryItem newPI = new PantryItem(item);
+		Pantry.addToPantry(newPI);
+		checklist.remove(item);
 	}
 	
-	// ----- Helper Methods ----- //
-	//Takes in a string, converts it to a PantryItem, and gives the new PantryItem to the Pantry
-	private void addToPantry(String newPI) {
-		
-	}
 }
