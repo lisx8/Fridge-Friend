9694df674a335c825d2fe449f3f0f47ba3e2f178
Talia <tsjacksonking@mta.ca>
2019-02-02 20:37:08 -0400

    Add files via upload

--- GroceryList.java
+++ GroceryList.java
@@ -3,46 +3,60 @@ import java.io.*;
 
 public class GroceryList {
 	// ----- Instance Variables ----- //
-	private static ArrayList<String> checklist;
-	
+	private static ArrayList<String> checkList = new ArrayList();
+	// !FLAG this file location should change if the install path for the .exe is changed. COME BACK TO THIS
+	private static File savedGroceryList = new File("C:/Program Files/Fridge-Friend/savedGroceryList.txt");
+
 	// ----- Constructor ----- //
 	public GroceryList() throws FileNotFoundException{
-		checklist = new ArrayList<String>();
+		checkList = new ArrayList<String>();
 		this.uploadChecklist();
 	}
-	
+
 	// ---- Methods ----- //
 	//Uploads the saved checklist by reading the checkList from a file
-	private void uploadChecklist() throws FileNotFoundException {
-		
+	private void uploadChecklist() throws FileNotFoundException{
+		Scanner sc = new Scanner(savedGroceryList);
+		while(sc.hasNext())
+		{
+			checkList.add(sc.next());
+		}
+		sc.close();
 	}
-	
+
 	//Saves the current checklist by printing checklist to a file
-	private void saveChecklist() {
-		
+	//Overwrites and saves the current checklist by printing checklist to a file
+	public void saveChecklist() throws FileNotFoundException{
+		PrintWriter wr = new PrintWriter(savedGroceryList);
+		for(int i = 0; i < checkList.size(); i++)
+		{
+			wr.print(checkList.remove(i));
+		}
+		wr.close();
 	}
-	
+
 	//Adds new item to the checklist
 	//In the future, we might make this boolean, because if the checklist already contains the item
 	//We'll want to tell the user it already exists
-	private static void addToChecklist(String newItem) {
+	public static void addToChecklist(String newItem) {
 		//Do we want the checklist sorted? if so, we would probably sort them as we add them to the checklist
-		if (!checklist.contains(newItem)) {
-			checklist.add(newItem);
+		if (!checkList.contains(newItem)) {
+			checkList.add(newItem);
 		}
 	}
-	
+
 	//Deletes the item without adding it to the Pantry
-	private static void deleteWithoutAddingToPantry(String item) {
-		checklist.remove(item);
+	public static void deleteWithoutAddingToPantry(String item) {
+		checkList.remove(item);
 	}
-	
+
 	//Deletes the item from the checklist
 	//and calls Pantry.addtoPantry(String) to add it to the Pantry
 	private static void deleteAndAddToPantry(String item) {
-		PantryItem newPI = new PantryItem(item);
-		Pantry.addToPantry(newPI);
-		checklist.remove(item);
+		Pantry.addToPantry(item);
+		checkList.remove(item);
+	//and called addtoPantry(String) to add it to the Pantry
 	}
-	
 }
+
+//https://stackoverflow.com/questions/10166453/how-to-save-something-to-the-desktop-without-hard-coding-the-directory/10166623#10166623
