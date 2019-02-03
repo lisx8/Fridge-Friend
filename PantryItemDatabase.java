34987b3b959e80c44f0099b11cf313163d112de1
opixe <47275861+opixe@users.noreply.github.com>
2019-02-03 00:01:07 -0400

    Update PantryItemDatabase.java

--- PantryItemDatabase.java
+++ PantryItemDatabase.java
@@ -1,5 +1,5 @@
 public class PantryItemDatabase {
-	private static DatabaseItem[] array = {new DatabaseItem("butter", 273), new DatabaseItem("cereal", 365), new DatabaseItem("chicken", 1), new DatabaseItem("Soy milk", 365)};
+	private static DatabaseItem[] array = {new DatabaseItem("butter", 273), new DatabaseItem("cereal", 365), new DatabaseItem("chicken", 1), new DatabaseItem("milk", 365)};
 	// note: will likely need to either readIn on launch from file or cloud OR 
 	// call to cloud / file each time search is performed - store in some sort of graph
 	// THE CURRENT IMPLEMENTATION IS FOR DEMONSTRATION PURPOSES ONLY
